9274d7ad9f865d94241413345a3876a200e63f63
fY-Hr <anteikuwute@gmail.com>
2026-08-22 07:10:59 +0700

    feat: game logic

--- backend/game.ts
+++ backend/game.ts
@@ -2,26 +2,80 @@ export interface Player{
 	id: string;
 	name: string;
 	hp: number;
+	afk: boolean;
+	dead: boolean;
 }
 
+export interface GameConfig{
+	maxHp: 1|2|3;
+}
+
+async function loadKbbiFile(): Promise<string[]>{
+	const file = Bun.file('./kbbi.txt');
+	const content = await file.text();
+	const result: string[] = content
+		.split(/\r?\n/)
+		.map((w: string) => w.trim())
+		.filter(Boolean); 
+	return result;
+}
+
+const wordList: string[] = await loadKbbiFile();
+export const kbbiSet = new Set<string>(wordList);
+
 export class Game{
-	currentWord = "mulai";
+	currentWord = "";
 	gameStatus = false;
-	lastChar = "i";
+	gameConfig: GameConfig = {
+		maxHp: 2
+	}
+	lastChar = "";
 	players: Player[] = [];
 	turnIndex = 0;
-	usedWords = Set<string>(["mulai"]);
+	usedWords = new Set<string>();
+	winner: Player | null = null;
 
-	// add player bisa untuk reconnect
-	addPlayer({id, name}: Omit<Player, 'hp'>): {success: boolean, message?: string}{
-		const gameRunning = this.gameStatus;
-		if(gameRunning){
-			return {success: false, message: "Game sedang berlangsung"}
+	startGame(gameConfig: GameConfig){
+		if(this.players.length < 2){
+			return {success: false, message: "Harus ada minimal 2 player"}
 		}
 
+		const randomWord = this.getRandomWord()
+
+		this.gameStatus = true;
+		this.players.forEach(p => {
+			p.hp = gameConfig.maxHp;
+			p.dead = false;
+		});
+		this.gameConfig = gameConfig;
+		this.turnIndex = 0;
+
+		this.usedWords.clear();
+		this.usedWords.add(randomWord);
+		this.currentWord = randomWord;
+		this.lastChar = randomWord.slice(-1);
+
+		this.nextTurn();
+	}
+
+	gameEnd(){
+		this.gameStatus = false;
+	}
+
+	getRandomWord(): string {
+		return wordList[Math.floor(Math.random() * wordList.length)] || "";
+	}
+
+	// add player bisa untuk reconnect
+	addPlayer({ id, name }: { id: string; name: string}): {success: boolean, message?: string}{
 		const existing = this.players.find(p => p.id === id);
 		if(existing){
 			return {success: true}
+		}
+
+		const gameRunning = this.gameStatus;
+		if(gameRunning){
+			return {success: false, message: "Game sedang berlangsung"}
 		}
 
 		const nameExist = this.players.find(p => p.name === name);
@@ -29,12 +83,68 @@ export class Game{
 			return {success: false, message: "Nama sudah digunakan oleh player lain"}
 		}
 
-		this.players.push({id, name, hp: 3});
+		this.players.push({id, name, hp: this.gameConfig.maxHp, afk: false, dead: false });
 		return {success: true}
 	}
 	
 	nextTurn(){
+		const players = this.players;
 
+		// check afk for first turn 
+		if(this.turnIndex == 0){
+
+			if(players[0]?.afk){
+				this.turnIndex = (this.turnIndex + 1) % players.length;
+			}
+
+			return this.turnIndex;
+		}
+
+		do{
+			this.turnIndex = (this.turnIndex + 1) % players.length;
+		} while (players[this.turnIndex]?.dead || players[this.turnIndex]?.afk){
+			this.turnIndex = (this.turnIndex + 1) % players.length;
+		}
+
+		return this.turnIndex;
 	}
 
+	submitAnswer(answer: string): {success: boolean, message?: string}{
+		const userWord = answer.toLowerCase();
+		const isValid = kbbiSet.has(userWord);
+		const isMatchingLastChar = userWord.slice(0, 1) === this.lastChar;
+		const isUsed = this.usedWords.has(userWord);
+		if(!isValid){
+			return {success: false, message: "Kata tidak ada dalam kbbi"}
+		}
+		if(!isMatchingLastChar){ 
+			return {success: false, message: "Kata tidak cocok dengan huruf terakhir"}
+		}
+		if(isUsed){
+			return {success: false, message: "Kata sudah digunakan"}
+		}
+		
+		this.usedWords.add(userWord);
+		this.currentWord = userWord;
+		this.lastChar = userWord.slice(-1);
+
+		this.nextTurn();
+
+		return {success: true};
+	}
+
+	// jika player afk kembali, maka pinaltinya adalah hp berkurang menyesuaikan hp player aktif yang memiliki hp paling rendah.
+	returnFromAfk(id: string): {success: boolean, message?: string}{
+		const player = this.players.find(p => p.id === id);
+		if(!player){
+			return {success: false, message: "Player tidak ada"}
+		}
+
+		const activePlayers = this.players.filter((p) => !p.dead && !p.afk && p.id != id);
+		const minHp = Math.min(...activePlayers.map(p => p.hp));
+		player.hp = minHp;
+		player.afk = false;
+
+		return {success: true};
+	}
 }
